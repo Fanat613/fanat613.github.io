dfbcb85e1385dfef50f65480586a5c55d419cc26
Sergii <gdetotam@i.ua>
2018-07-19 21:00:49 +0300

    add images

--- my-site/src/App/App.js
+++ my-site/src/App/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../common/style/reset.css'
+import '../common/style/base.css'
+
+
+
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
 
 class App extends Component {
   render() {
